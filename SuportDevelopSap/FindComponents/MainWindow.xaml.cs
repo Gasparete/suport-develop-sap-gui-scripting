a494425c785db9e5ffbd570a4f4a2b171e536857
Vitor Gasparete dos Anjos Cabral <Vitorgasparete@hotmail.com>
2022-12-16 08:53:50 -0300

    First commit
    Commit with innitial settings and first funcioality (Find components by ID).

---
+++ SuportDevelopSap/FindComponents/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using SAPFEWSELib;
+using SapROTWr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Services;
+
+namespace FindComponents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Wpf.Ui.Controls.UiWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIdentify_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CSapROTWrapper sapROTWrapper = new();
+                object sapGuilRot = sapROTWrapper.GetROTEntry("SAPGUI");
+                GuiApplication application = (GuiApplication)sapGuilRot.GetType().InvokeMember("GetScriptingEngine", System.Reflection.BindingFlags.InvokeMethod, null, sapGuilRot, null);
+                GuiConnection conn = application.Connections.Item(0) as GuiConnection;
+                GuiSession ses = conn.Sessions.Item(0) as GuiSession;
+
+                ((GuiVComponent)ses.FindById(txtIdentify.Text)).SetFocus();
+            }
+            catch (Exception)
+            {
+                Wpf.Ui.Controls.MessageBox messageBox = new Wpf.Ui.Controls.MessageBox();
+
+                MessageBox.Show("Não foi possível localizar o componente.", "Alerta!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }   
+        }
+    }
+}
